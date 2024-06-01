a367fcf1fc24d2cb5897f238d2a5b42c1e8c7d72
combizera <ycombi349@gmail.com>
2024-06-01 19:28:03 -0300

    profileUpdatedRequest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Testando Husky</title>
+        <title>Profile Updated Request</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
