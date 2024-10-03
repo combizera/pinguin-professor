f053f442864e8242b8e92bb4c09a2cd98b61ace5
combizera <ycombi349@gmail.com>
2024-10-03 20:18:47 -0300

    feat: create question and update DBS

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name'  => 'Test User',
-            'email' => 'test@example.com',
+            'name'     => 'Test User',
+            'email'    => 'admin@advbox.com.br',
+            'password' => 'admin@advbox.com.br',
         ]);
 
         Question::factory()->count(10)->create();
