75251dccfd83aad738cb1e7174db69530987a29a
combizera <ycombi349@gmail.com>
2024-10-03 21:47:18 -0300

    wip: clean code

--- tests/Feature/Question/PublishTest.php
+++ tests/Feature/Question/PublishTest.php
@@ -9,7 +9,10 @@ use function Pest\Laravel\put;
 //it('should be able to publish a question', function () {
 //    //    Arrange :: preparar
 //    $user     = User::factory()->create();
-//    $question = Question::factory()->create(['draft' => true, 'created_by' => $user->id]);
+//    $question = Question::factory()->create([
+//        'draft' => true,
+//        'created_by' => $user->id,
+//        ]);
 //    actingAs($user);
 //
 //    //    Act :: agir
@@ -21,21 +24,26 @@ use function Pest\Laravel\put;
 //    expect($question)
 //        ->draft->toBeFalse();
 //});
-//
+
 //it('should make sure that only the person who has create the question can publish the question', function () {
 //    //    Arrange :: preparar
 //    $rightUser = User::factory()->create();
 //    $wrongUser = User::factory()->create();
-//    $question  = Question::factory()->create(['draft' => true, 'created_by' => $rightUser->id]);
+//    $question  = Question::factory()->create([
+//        'draft' => true,
+//        'created_by' => $rightUser->id,
+//        ]);
 //
 //    //    Act :: agir
 //    actingAs($wrongUser);
+//
 //    //    Assert :: verificar
 //    put(route('question.publish', $question))
 //        ->assertForbidden();
 //
 //    //    Act :: agir
 //    actingAs($rightUser);
+//
 //    //    Assert :: verificar
 //    put(route('question.publish', $question))
 //        ->assertRedirect();
